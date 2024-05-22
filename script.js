911c386f5f4f8a4bdd38a7691ffaec7c57f37299
ledsouza <leandro.souza.159@gmail.com>
2024-05-22 14:12:26 -0300

    adicionada funcionalidade de filtro por categoria

--- script.js
+++ script.js
@@ -1,44 +1,14 @@
 const containerVideos = document.querySelector(".videos__container");
 const barraDePesquisa = document.querySelector(".pesquisar__input");
-
-async function buscarEMostrarVideos() {
-    try {
-        const busca = await fetch("http://localhost:3000/videos");
-        if (!busca.ok) {
-            throw new Error(`HTTP error: ${busca.status}`);
-        }
-        const videos = await busca.json();
-        videos.forEach((video) => {
-            containerVideos.innerHTML += `
-                    <li class="videos__item">
-                        <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allow="fullscreen"></iframe>
-                        <div class="descricao-video">
-                            <img class="img-canal" src="${video.imagem}" alt="Logo do canal">
-                            <h3 class="titulo-video">${video.titulo}</h3>
-                            <p class="titulo-canal">${video.descricao}</p>
-                        </div>
-                    </li>
-                    `;
-        });
-    } catch (error) {
-        containerVideos.innerHTML = `<p>Ocorreu um erro ao carregar os vídeos: ${error}</p>`;
-    }
-}
+const botoes = document.querySelectorAll(".superior__item");
 
 buscarEMostrarVideos();
 
-function filtrarVideos() {
-    const tituloFiltro = barraDePesquisa.value.toLowerCase();
-    const videos = document.querySelectorAll(".videos__item");
-
-    videos.forEach((video) => {
-        let titulo = video.querySelector(".titulo-video").textContent.toLowerCase();
-        video.style.display = tituloFiltro
-            ? titulo.includes(tituloFiltro)
-                ? "block"
-                : "none"
-            : "block";
-    });
-}
-
 barraDePesquisa.addEventListener("input", filtrarVideos);
+
+botoes.forEach((botao) => {
+    let nomeCategoria = botao.getAttribute("name").toLowerCase();
+    botao.addEventListener("click", () => {
+        filtrarPorCategoria(nomeCategoria);
+    });
+});
